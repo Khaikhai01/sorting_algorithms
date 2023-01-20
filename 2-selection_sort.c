c484178d39517254ffd294bb86023b25cb809151
njogued <njogued@gmail.com>
2023-01-20 05:17:20 -0800

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * selection_sort - FUnction for selection sort
+ * @array: Array to sort
+ * @size: The size of the array
+ * Return: Nothing
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t index = 0;
+	int max = 0, temp = 0, diff = 0;
+	size_t min_index = 0, j = 0;
+
+	for (index = 0; index < size; index++)
+	{
+		max = 0;
+		min_index = index;
+		for (j = index; j < size; j++)
+		{
+			diff = array[index] - array[j];
+			if (diff > max)
+			{
+				max = diff;
+				min_index = j;
+			}
+		}
+		if(max > 0)
+		{
+			temp = array[min_index];
+			array[min_index] = array[index];
+			array[index] = temp;
+			print_array(array, size);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -19,6 +19,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+void selection_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
