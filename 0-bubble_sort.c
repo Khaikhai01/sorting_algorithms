4c02de53112c0df37645bfd458d5573f98b87df4
njogued <njogued@gmail.com>
2023-01-24 11:19:12 -0800

    Tasks

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,22 +8,22 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t iterator = 0, reverser = 0;
-    int temp = 0, counter = 1;
+	size_t iterator = 0, reverser = 0;
+	int temp = 0, counter = 1;
 
-    for(reverser = 0; reverser < size && counter != 0; reverser++)
-    {
-        counter = 0;
-        for(iterator = 0; iterator < size - 1; iterator++)
-        {
-            if(array[iterator] > array[iterator + 1])
-            {
-                counter++;
-                temp = array[iterator];
-                array[iterator] = array[iterator + 1];
-                array[iterator + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (reverser = 0; reverser < size && counter != 0; reverser++)
+	{
+		counter = 0;
+		for (iterator = 0; iterator < size - 1; iterator++)
+		{
+			if (array[iterator] > array[iterator + 1])
+			{
+				counter++;
+				temp = array[iterator];
+				array[iterator] = array[iterator + 1];
+				array[iterator + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
