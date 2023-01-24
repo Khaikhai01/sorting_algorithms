f4f6d9d2d673de0f0267f27688b9a6c967465471
Khaikhai01 <shishchei@gmail.com>
2023-01-25 00:08:22 +0300

    added prototypes

--- sort.h
+++ sort.h
@@ -19,6 +19,12 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
+void insertion_sort_list(listint_t **list);
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
